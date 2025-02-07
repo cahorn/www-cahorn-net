be5eb0ddb581817a734a516de60146e5f0361b7e
Ansel Horn <dev@cahorn.net>
2025-02-07 09:50:12 -0500

    Apply Black formatting standard to pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,12 @@
-AUTHOR = 'Ansel Horn'
-SITENAME = 'Ansel Horn'
+AUTHOR = "Ansel Horn"
+SITENAME = "Ansel Horn"
 SITEURL = ""
 
 PATH = "content"
 
-TIMEZONE = 'America/New_York'
+TIMEZONE = "America/New_York"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
